9c2f1e8918b3105c59e2a308900bf22ea0e98cec
Pelinnn <81982628+pelinkols@users.noreply.github.com>
2021-04-07 00:09:01 +0300

    HW1- PK

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,23 @@
-#Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+
+'''I defined two lists; one contains the odd numbers, the other one contains the even numbers 1 to 10. 
+After that, I merged two lists and named it "newlist". 
+I created a new list called squares to be able to multiply all values in newlist by 2. I used the "square.sort" due to sorting the values in increasing order.
+Finally, I used for loop to print the data type of all values in the new list. '''
+
+
+list_a=[1,3,5,7,9]
+list_b=[2,4,6,8,10]
+squares = []
+
+newlist= list_a + list_b
+
+squares = [i*2 for i in newlist]
+squares.sort
+print(squares)
+
+for i in newlist:
+    print(type(newlist))
+
+
