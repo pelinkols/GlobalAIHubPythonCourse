b4e33915eadf59e88588080be0823796934a4c40
Pelinnn <81982628+pelinkols@users.noreply.github.com>
2021-04-10 19:23:21 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,26 @@
-#Explain your work
 
-#Question 1
-for x in range(a):
-	print(a)
+#Homework Day2
+
+#6,7,8,9,10,1,2,3,4,5
+
+listem = ['1','2','3','4','5',
+          '6','7','8','9','10']
+
+
+print(list((listem[-5:]))+listem[:-5])
+
+#2nd part of the homework
+    
+n=int(input("Please enter a single digit:"))
+p=0
+
+while not 0<n<10:
+    print("please enter one digit number")
+    n=int(input("Please enter a single digit:"))
+    
+    
+for i in range(n+1):
+    if i%2==0:
+        print(i)
+        p+=1
+print("There are",p, "even numbers from O to",n)
